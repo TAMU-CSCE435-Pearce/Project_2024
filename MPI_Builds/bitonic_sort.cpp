a5774c20e70228a01cdba620dd5a62a083e950c0
DarwinWhite <91921582+DarwinWhite@users.noreply.github.com>
2024-10-15 10:33:51 -0500

    Newer implementation added

--- MPI_Builds/bitonic_sort.cpp
+++ MPI_Builds/bitonic_sort.cpp
@@ -268,251 +268,225 @@
 //     return 0;
 // }
 
-#include <omp.h>
-#include "mpi.h"
-#include <stddef.h>
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
-#include <sys/timeb.h>
-#include <ctime>
+#include <sys/time.h>
+#include <math.h>
+#include "mpi.h"
 
-#define SWAP(x,y) t = x; x = y; y = t;
+struct timeval startwtime, endwtime;
+double seq_time;
 
-using namespace std;
+int cmpfuncASC (const void * a, const void * b);
+int cmpfuncDESC (const void * a, const void * b);
+void init(int* a,int seed);
 
-int up = 1;
-int down = 0;
+int N;
+void test(int* a, int num_tasks,int rank);
+void compute_top(int partner, int dir,int *a,int rank, int k);
+void compute_bottom(int partner,int dir,int *a,int rank , int k);
+inline void swap(int *a, int *b);
 
-void init(void);
-void print(void);
-int N, m, numThreads;
-int i, s;
 
-void compare(int* arr, int i, int j, int dir)
-{
-	int t;
-	if (dir == (arr[i] > arr[j])) {
-		SWAP(arr[i], arr[j]);
-	}
+/** the main program **/
+int main(int argc, char **argv) {
+
+  if (argc != 2) {
+    printf("Usage: %s q\n  where n=2^q is problem size (power of two)\n",argv[0]);
+    exit(1);
+  }
+  int  numtasks, rank, rc,i,j, *a, k, dir;
+  
+  rc = MPI_Init(&argc,&argv);
+  if (rc != MPI_SUCCESS) {
+     printf ("Error starting MPI program. Terminating.\n");
+     MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank); 
+  
+  N = (1<<atoi(argv[1]))/numtasks;
+  a = (int *) malloc(N * sizeof(int));  
+  init(a,rank);  
+  //start counting time
+  gettimeofday (&startwtime, NULL);
+  if ((rank%2 == 0) ) qsort(a, N, sizeof(int), cmpfuncASC); //ASCENDING
+  else qsort(a, N, sizeof(int), cmpfuncDESC); //DESCENDING
+  MPI_Barrier(MPI_COMM_WORLD);
+  
+  for (k=1;k<numtasks;k<<=1){
+    dir = ((k*2 & rank) == 0);
+   
+    for (j=k;j>=1;j>>=1){
+      int partner = rank^j;  
+      if(rank<partner) compute_bottom(partner,dir,a,rank,j);
+      else compute_top(partner,dir,a,rank,j);
+      MPI_Barrier(MPI_COMM_WORLD);
+    }
+    if (dir) qsort(a, N, sizeof(int), cmpfuncASC); //ASCENDING
+    else qsort(a, N, sizeof(int), cmpfuncDESC); //DESCENDING
+  }
+  //program finished
+  gettimeofday (&endwtime, NULL);
+  seq_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+		      + endwtime.tv_sec - startwtime.tv_sec);
+
+  if(rank == 1) printf("Parallel bitonic sort time = %f\n", seq_time);
+  test(a,numtasks,rank);
 }
 
-/*
-* Sorts a bitonic sequence in ascending order if dir=1
-* otherwise in descending order
-*/
-void bitonicmerge(int* arr, int start, int length, int dir)
-{
-	int halfLength, i;
-	if (length > 1)
-	{
-		halfLength = length / 2;
-		for (i = start; i < start + halfLength; i++)
-			compare(arr, i, i + halfLength, dir);
-		m++;
-#pragma omp parallel sections
-		{
-#pragma omp section
-			bitonicmerge(arr, start, halfLength, dir);
-#pragma omp section
-			bitonicmerge(arr, start + halfLength, halfLength, dir);
-		}
-	}
+/** function called by the smaller partner to swap the bottom part
+    of both tables 
+**/
+void compute_bottom(int partner, int dir,int *a,int rank, int k){
+  MPI_Status mpistat;
+  int rc,i;
+  int *swap_temp = (int *)malloc(N/2*sizeof(int));
+  //tag = 1 means you need to proccess the message. 
+  //since my job is to compute the top part, im sending the bottom for the other guy.
+  rc = MPI_Send(a+N/2,N/2,MPI_INT,partner,1,MPI_COMM_WORLD);
+  if (rc != MPI_SUCCESS) {
+    printf ("Error idle. Terminating.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  //i need to compare this to my bottom half.
+  MPI_Recv(swap_temp,N/2,MPI_INT,partner,1,MPI_COMM_WORLD,&mpistat);
+  if(dir){
+    for(i=0;i<N/2;i++){
+      if(a[i] > swap_temp[i]) swap(&a[i],&swap_temp[i]);  
+    }
+   
+  }
+  else{
+    for(i=0;i<N/2;i++){
+      if(a[i] < swap_temp[i]) swap(&a[i],&swap_temp[i]);
+    }
+  }
+  //this guy sends the results before receiving
+  rc = MPI_Send(swap_temp,N/2,MPI_INT,partner,2,MPI_COMM_WORLD);
+  if (rc != MPI_SUCCESS) {
+    printf ("Error idle. Terminating.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  //receiving the computed top part from the partner.
+  MPI_Recv(a+N/2,N/2,MPI_INT,partner,2,MPI_COMM_WORLD,&mpistat);
+}  
+
+
+/** function called by the bigger partner to swap the top part
+    of both tables 
+**/
+void compute_top(int partner, int dir,int *a,int rank, int k) {
+  MPI_Status mpistat;
+  int rc,i;
+  int *swap_temp = (int *)malloc(N/2*sizeof(int));
+  //this guy will receive before sending. If they both attemp to send first it may lead to a deadlock.
+  MPI_Recv(swap_temp,N/2,MPI_INT,partner,1,MPI_COMM_WORLD,&mpistat);
+  rc = MPI_Send(a,N/2,MPI_INT,partner,1,MPI_COMM_WORLD);
+  if (rc != MPI_SUCCESS) {
+    printf ("Error idle. Terminating.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  if(dir){ 
+      for(i=0;i<N/2;i++){
+	if(a[i+N/2] < swap_temp[i]) swap(&a[i+N/2],&swap_temp[i]);
+      }   
+  }
+  else{
+    for(i=0;i<N/2;i++){
+      if(a[i+N/2] > swap_temp[i]) swap(&swap_temp[i],&a[i+N/2]);
+    }
+  }
+  //Proccessing has ended. This guy will first receive and then send to avoid deadlock.
+  MPI_Recv(a,N/2,MPI_INT,partner,2,MPI_COMM_WORLD,&mpistat);
+  rc = MPI_Send(swap_temp,N/2,MPI_INT,partner,2,MPI_COMM_WORLD);
+  if (rc != MPI_SUCCESS) {
+    printf ("Error idle. Terminating.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+  
 }
 
-/*
-* Generates bitonic sequence by sorting recursively
-* two halves of the array in opposite sorting orders
-* bitonicmerge will merge the resultant data
-*/
-void recbitonic(int* arr, int start, int length, int dir)
-{
-	int halfLength;
-	if (length > 1)
-	{
-		halfLength = length / 2;
-		recbitonic(arr, start, halfLength, up);
-		recbitonic(arr, start + halfLength, halfLength, down);
-		bitonicmerge(arr, start, length, dir);
-	}
+// the first element must be the lowest target number
+inline void swap(int *a, int *b){
+
+  int t;
+  t = *a;
+  *a = *b;
+  *b = t;
 }
 
-int* changeSizeArray(int* arr, int lenght, int newLenght)
-{
-	int *array = new int[newLenght];
-	for (int i = 0; i < lenght; i++)
-	{
-		array[i] = arr[i];
-	}
-	delete[]arr;
-	return array;
+void init(int* a,int seed) {
+  int i;
+  srand(seed+3);
+  for (i = 0; i < N; i++) {
+    a[i] = rand() % (4*N);
+  }
 }
-int main(int argc, char **argv){
-	int ProcRank, ProcNum;
-	int* data;
-	//int* curArrSorted;
-	data = (int *)malloc(N * sizeof(int));
-	int curCountArr = 0;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
-	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	MPI_Status status;
+int cmpfuncASC (const void * a, const void * b)
+{
+   return ( *(int*)a - *(int*)b );
+}
 
-	omp_set_nested(0);
-	N = (argc > 1) ? atoi(argv[1]) : 8;
-	m = (argc > 2) ? atoi(argv[2]) : 100;
-	numThreads = (argc > 3) ? atoi(argv[3]) : omp_get_max_threads();
+int cmpfuncDESC (const void * a, const void * b)
+{
+   return ( *(int*)b - *(int*)a );
+}
+void test(int* a, int num_tasks,int rank)
+{
+  int flag=1;
+  int i,rc;
+  int min = a[0];
+  int max = 0;
 
-	printf("Threads count: %d\n", numThreads);
-	omp_set_num_threads(numThreads);
-
-	//partSize = N / numThreads;
-	//init();
-	//print();
-	//data[0] = 1;
-	//data[1] = 4;
-	//data[2] = 7;
-	//data[3] = 9;
-	//data[4] = 12;
-	//data[5] = 6;
-	//data[6] = 3;
-	//data[7] = 2;
-
-	int* Offsets = new int[ProcNum];
-	int* Elements = new int[ProcNum];
-	int part;
-
-	if (!ProcRank)
-	{
-
-		for (i = 0; i < N; i++) {
-			data[i] = rand() % m;
-			//cout << data[i] << " ";
-		}
-
-		cout << "Data generated" << endl;
-	}
-
-	if (ProcNum % 2 == 0) {
-		cout << "Odd number" << endl;
-		part = N / ProcNum;
-		cout << "Part: " << part << endl;
-
-		for (int i = 0; i < ProcNum; i++)
-		{
-			Offsets[i] = i*part;
-			Elements[i] = part;
-			//cout << "Offset is: " << Offsets[i] << " For i:" << i << endl;
-			//cout << "Elements is: " << Elements[i] << " For i:" << i << endl;
-		}
-	}
-	else {
-		cout << "Even number" << endl;
-		int free = N / 2;
-
-		if (ProcNum == 1) {
-			Elements[0] = N;
-			Offsets[0] = 0;
-		}
-		else
-		{
-			int sum = 0;
-			for (int i = 0; i < ProcNum; i++)
-			{
-				Elements[i] = free;
-				if (i == ProcNum - 1) {
-					Elements[i] = Elements[i - 1];
-					Offsets[i] = sum;
-					cout << "Offset is: " << Offsets[i] << " For i:" << i << endl;
-					cout << "Elements is: " << Elements[i] << " For i:" << i << endl;
-					break;
-				}
-				free /= 2;
-				Offsets[i] = sum;
-				sum += Elements[i];
-				cout << "Offset is: "<< Offsets[i] << " For i:"<< i << endl;
-				cout << "Elements is: " << Elements[i] << " For i:" << i << endl;
-			}
-		}
-	}
-	int currArrSize = Elements[ProcRank];
-	//cout << "ProcRank: " << ProcRank << " Size: " << currArrSize << endl;
-	double startTime = MPI_Wtime();
-	int* curArrSorted = new int[Elements[ProcRank]];
-	MPI_Scatterv(data, Elements, Offsets, MPI_INT, curArrSorted, Elements[ProcRank], MPI_INT, 0, MPI_COMM_WORLD);
-	
-	recbitonic(curArrSorted, 0, currArrSize, ProcRank % 2 == 0 ? up : down);
-
-	//собирание младшими процессами от старших
-	int lastProc = -1;
-	for (int i = 2; ProcNum / i > 0; i *= 2)
-	{
-		for (int j = ProcNum - 1 - ((i - 2) / 2); j >= 0; j -= i)
-		{
-			if ((ProcRank == j) && (ProcRank != 0)) //sender
-			{
-				MPI_Send(&currArrSize, 1, MPI_INT, (j - i / 2 < 0) ? 0 : j - i / 2, 0, MPI_COMM_WORLD);
-				MPI_Send(curArrSorted, currArrSize, MPI_INT, (j - i / 2 < 0) ? 0 : j - i / 2, 0, MPI_COMM_WORLD);
-				//cout << ProcRank << " Sended message for: " << (j - i / 2 < 0) ? 0 : j - i / 2;
-			}
-			if ((ProcRank == j - i / 2) || ((j - i / 2 < 0) && (ProcRank == 0) && (j != 0)))//receiver
-			{
-				int sizeRecv = 0;
-				MPI_Recv(&sizeRecv, 1, MPI_INT, j, 0, MPI_COMM_WORLD, &status);
-				curArrSorted = changeSizeArray(curArrSorted, currArrSize, currArrSize + sizeRecv);
-				MPI_Recv(curArrSorted + currArrSize, sizeRecv, MPI_INT, j, 0, MPI_COMM_WORLD, &status);
-				
-				currArrSize += sizeRecv;
-
-				cout << "i + j " << i/2 + j << endl;
-				bitonicmerge(curArrSorted, 0, currArrSize, (j + i / 2) % 2 == 0 ? 0 : 1);
-
-				if (ProcRank != 0 && ProcNum / (2 * i) == 0)
-					lastProc = ProcRank;
-			}
-		}
-	}
-	if (ProcRank == lastProc && ProcRank != 0)
-	{
-		//cout << "Lat proc: " << ProcRank << endl;
-		MPI_Send(&currArrSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-		MPI_Send(curArrSorted, currArrSize, MPI_INT, 0, 0, MPI_COMM_WORLD);
-	}
-	if (ProcRank == 0)
-	{
-		int sizeRecv = 0;
-		if (lastProc != 0 && lastProc != ProcNum)
-		{
-			MPI_Recv(&sizeRecv, 1, MPI_INT, lastProc, 0, MPI_COMM_WORLD, &status);
-			curArrSorted = changeSizeArray(curArrSorted, currArrSize, currArrSize + sizeRecv);
-			MPI_Recv(curArrSorted + currArrSize, sizeRecv, MPI_INT, lastProc, 0, MPI_COMM_WORLD, &status);
-			int size = currArrSize + sizeRecv;
-			bitonicmerge(curArrSorted, 0, size, 1);
-		}
-		double endTime = MPI_Wtime();
-		printf("Time of sort: %f \n", endTime - startTime);
-		if (N < 128) {
-			cout << "Sorted array: " << endl;
-			for (int i = 0; i < N; i++) {
-				cout << curArrSorted[i] << ' ';
-			}
-		}
-		
-	}
-
-	//startTime = clock();
-	//sort();
-	//bitonicmerge(0, 8, 1);
-	//endTime = clock();
-	//printf("Time: %f", (endTime - startTime) / CLOCKS_PER_SEC);
-	/*if (N < 128) {
-		printf("\n");
-		for (i = 0; i < N; i++) {
-			printf("%d ", data[i]);
-		}
-	}
-	getchar();*/
-	MPI_Finalize();
-	return 0;
-};
+  
+  for(i=0;i<N-1;i++){
+    if (a[i+1] < a[i]) flag = 0;
+    if (a[i] > max) max = a[i];
+    if (a[i] < min) min = a[i];
+  }
+  if(rank == 0){
+   
+    MPI_Status mpistat;
+    int other_flag;
+    int *minimum = (int *)malloc(num_tasks*sizeof(int));
+    int *maximum = (int *)malloc(num_tasks*sizeof(int));
+    minimum[0] = min;
+    maximum[0] = max;
+    for(i=1;i<num_tasks;i++){
+      //tag = 3 means local sorting flag
+      MPI_Recv(&other_flag,1,MPI_INT,i,3,MPI_COMM_WORLD,&mpistat);
+      flag = flag & other_flag;
+      //tag = 4 means minimum
+      MPI_Recv(minimum+i,1,MPI_INT,i,4,MPI_COMM_WORLD,&mpistat);
+      //tag = 5 means maximum
+      MPI_Recv(maximum+i,1,MPI_INT,i,5,MPI_COMM_WORLD,&mpistat);
+    }
+    if(flag) printf("everyone is locally sorted\n");
+    else {
+      printf("at least one failed to sort himself locally\n");
+      exit(1);
+    }
+    //from now on flag has a new interpretetion
+    for(i=0;i<num_tasks-1;i++){
+      if(maximum[i] > minimum[i+1]) flag = 0;   
+    }
+    //if flag is still 1 at this point, test has been passed
+    if(flag) printf("parallel bitonic sort was a success\n");
+    else printf("bitonic sort has failed\n");
+    
+    
+  } 
+  else{
+    //tag = 3 means local sorting flag
+    rc = MPI_Send(&flag,1,MPI_INT,0,3,MPI_COMM_WORLD);
+    //tag = 4 means minimum
+    rc = MPI_Send(&min,1,MPI_INT,0,4,MPI_COMM_WORLD);
+    //tag = 5 means maximum
+    rc = MPI_Send(&max,1,MPI_INT,0,5,MPI_COMM_WORLD);
+     
+    
+  } 
+  
+}
